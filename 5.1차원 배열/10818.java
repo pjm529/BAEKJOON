23eb3496f6f0f19247a9bbc73e9c414995feaefb
Park Jun Mo <pok_nima@naver.com>
2022-01-21 15:13:14 +0900

    최솟값과 최댓값을 찾는 문제
    N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.

---
+++ 5.1차원 배열/10818.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr;
+		arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		sc.close();
+		Arrays.sort(arr);
+
+		System.out.println(arr[0] + " " + arr[N - 1]);
+	}
+}
