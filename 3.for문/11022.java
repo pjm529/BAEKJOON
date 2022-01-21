3a4d2aa832328f0290f3c5e7372178ebac7428fc
Park Jun Mo <pok_nima@naver.com>
2022-01-21 15:08:00 +0900

    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
    첫째 줄에 테스트 케이스의 개수 T가 주어진다.
    
    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)

---
+++ 3.for문/11022.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int a;
+		int b;
+		StringTokenizer st;
+
+		for (int i = 1; i < n + 1; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			bw.write("Case #" + i + ": " + a + " + " + b + " = " + (a + b) + "\n");
+		}
+
+		br.close();
+
+		bw.flush();
+		bw.close();
+	}
+}
