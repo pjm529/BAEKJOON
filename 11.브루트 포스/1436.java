5d3b689749ceef4f13253ad884d3f752834faec1
Park Jun Mo <pok_nima@naver.com>
2022-01-26 11:46:04 +0900

    N번째 종말의 수가 나올 때까지 차례대로 시도하는 문제

---
+++ 11.브루트 포스/1436.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int num = 666;
+		int count = 1;
+
+		while (count != N) {
+			num++;
+			if (String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+
+		System.out.println(num);
+		br.close();
+	}
+}
